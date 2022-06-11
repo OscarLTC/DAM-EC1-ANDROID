4f2a98cd4290ca47c58f1f686502d9d2ff414ef9
Oscar <oscartaco2003@gmail.com>
2022-06-10 20:53:59 -0500

    first commit

---
+++ app/src/main/java/tco/idat/ec01a/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package tco.idat.ec01a.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tco.idat.ec01a.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
